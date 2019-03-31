da7d1f2ae023f5f1ad34e2cb175134313ef59a33
CNLHC <2463765697@qq.com>
2019-03-31 14:46:01 +0800

    hot fix

--- tcpserver/SSCDecoder.py
+++ tcpserver/SSCDecoder.py
@@ -7,17 +7,21 @@ class CRCError(Exception):
 
 def SSCDecodeFrame(frame: bytes):
     hash = crc8.crc8()
+
+
     hash.update(frame[0:11])
+
+    print("Check Frame",(frame))
+
     if(hash.digest() != frame[-1:]):
-        print("CRC Check Error!", binascii.b2a_hex(
-            hash.digest()), binascii.b2a_hex(frame[-1:]))
+        print("CRC Check Error!",(hash.digest()),(frame[-1:]))
         raise CRCError
 
     payload = {
-        'RFID': binascii.b2a_hex(frame[0:4]),
-        'CartID':binascii.b2a_hex(frame[4:7]),
-        'op':binascii.b2a_hex(frame[7:8]),
-        'opData':binascii.b2a_hex(frame[8:11])
+        'RFID':(frame[0:4]),
+        'CartID':(frame[4:7]),
+        'op':(frame[7:8]),
+        'opData':(frame[8:11])
     }
 
     print(payload)
