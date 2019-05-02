8cd61a950e3e142edf0a56f555cefcb9f8e4d9ab
CNLHC <2463765697@qq.com>
2019-05-02 23:53:11 +0800

    添加mqtt-client,现在已经可以向broker推送消息

--- UHDServer/driver/source/main.cpp
+++ UHDServer/driver/source/main.cpp
@@ -4,41 +4,76 @@
 
 #include "UM2xx.h"
 #include <algorithm>
+#include <chrono>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <thread>
-#include <chrono>
+#include "mqtt/async_client.h"
+#include "mqttclient.hpp"
 
 using namespace std;
 
-int main() {
+const std::string DFLT_SERVER_ADDRESS   { "tcp://localhost:1883" };
+const std::string DFLT_CLIENT_ID        { "UHDDriver" };
+const string TOPIC { "hello" };
+const int  QOS = 1;
+const auto TIMEOUT = std::chrono::seconds(10);
+
+const char *LWT_PAYLOAD = "Last will and testament.";
+
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+void ReadThread() {
   um2xx::UM2xx umObj;
   umObj.connect(string("/dev/ttyS2"));
-  while(1){
-    while(1){
+  while (true) {
+    while (true) {
       auto data = umObj.CMDFindTagSingle(1000);
-      cout<<umObj.DataToHexString(data)<<endl;
-      if(data.size()>10)
+      if (data.size() > 10) {
+        cout << umObj.DataToHexString(data) << endl;
         break;
+      }
     }
     auto data = umObj.CMDFindTags(500);
-    for (auto &j :data){
-      cout<<"标签:"<<j.first<<"\t次数:"<<j.second<<endl;
-    }
-    cout<<"共获得标签"<<data.size()<<"个"<<endl;
-    cout<<"done";
-    std::this_thread::sleep_for(0.05s);
-
-
+    std::this_thread::sleep_for(0.5s);
   }
+}
+#pragma clang diagnostic pop
+
+int main() {
+  string address =  DFLT_SERVER_ADDRESS,
+         clientID = DFLT_CLIENT_ID;
+
+  cout << "Initializing for server '" << address << "'..." << endl;
+  mqtt::async_client client(address, clientID);
+
+  callback cb;
+  client.set_callback(cb);
+
+  mqtt::connect_options conopts;
+  mqtt::message willmsg(TOPIC, LWT_PAYLOAD, 1, true);
+  mqtt::will_options will(willmsg);
+  conopts.set_will(will);
+  cout << "  ...OK" << endl;
 
 
+  cout << "\nConnecting..." << endl;
+  mqtt::token_ptr conntok = client.connect(conopts);
+  cout << "Waiting for the connection..." << endl;
+  conntok->wait();
+  cout << "  ...OK" << endl;
+
+  cout << "\nSending message..." << endl;
+  mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC,"Test from CNLHC");
+  pubmsg->set_qos(QOS);
+  client.publish(pubmsg)->wait_for(TIMEOUT);
+  cout << "  ...OK" << endl;
+
+  thread readWorker(ReadThread);
+  readWorker.join();
+  return 0;
 
 }
-//3000E28068940000500380417C8400D201
-//3400300833B2DDD901400000000000C301
-
-
-//30 00 00 B0 7A 13 EC 36 F4 C8 08 00 37 B5 00 BA 01
